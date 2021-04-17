91d57024fb83ff73532a20679777dc6955c1db54
tanupoo <github-shoichi@tanu.org>
2021-04-17 21:48:56 +0900

    modified according to the recent change.

--- test.py
+++ test.py
@@ -4,17 +4,24 @@ word_file = "word_list.txt"
 import codeNwords
 
 cwm = codeNwords.CodeWordMap(word_file)
+
+print("## generation")
 ret = cwm.gencode()
 print(ret)
 print(cwm.code2words(ret["code"]))
 print(cwm.words2code(ret["words"]))
 
+print("## conversion")
 words = "さわぐ-もでる-くつした"
-expected = "500-897-234"
+expected = "497-892-234"
 code = cwm.words2code(words)
-print(f"{words} -> {code}: expected {expected}")
+print(f"{words} -> {code}: result {code==expected}: expected {expected}")
+if code != expected:
+    raise ValueError(f"{code} != {expected}")
 
 code = "324-243-133"
 expected = "たてる-ゆうはん-ごめん"
 words = cwm.code2words(code)
-print(f"{code} -> {words}: expected {expected}")
+print(f"{code} -> {words}: result {words==expected}: expected {expected}")
+if words != expected:
+    raise ValueError(f"{words} != {expected}")
